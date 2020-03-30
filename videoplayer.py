a94a004d426d10a5ccb1a3ced816fb5a6b27f3c1
Dino Gržinić <dino.grzinic@skole.hr>
2020-03-30 15:52:52 +0200

    Add files via upload

---
+++ videoplayer.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import pygame as pg
+pg.mixer.init()
+pg.mixer.music.load('dr stone op.mp3')
+pg.mixer.music.play()
+win=pg.display.set_mode((960,540),pg.RESIZABLE)
+# Create a VideoCapture object and read from input file
+cap = cv2.VideoCapture('Super Mario REMASTERED Opening.mp4')
+if (cap.isOpened()== False): 
+  print("Error opening video stream or file")
+# Read until video is completed
+while(cap.isOpened()):
+  # Capture frame-by-fram
+  ret, frame = cap.read()
+  if ret == True:
+    # Display the resulting frame
+    cv2.imshow('Frame',frame)
+    # Press Q on keyboard to  exit
+
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+      break
+  # Break the loop
+  else: 
+    break
+# When everything done, release the video capture object
+cap.release()
+# Closes all the frames
+#cv2.destroyAllWindows()
+
+
