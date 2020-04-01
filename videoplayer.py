5b455fd0f8d08dd7ffaa39a48ddedb5400c1dd99
Dino Gržinić <dino.grzinic@skole.hr>
2020-04-01 22:37:43 +0200

    Add files via upload

--- videoplayer.py
+++ videoplayer.py
@@ -2,11 +2,13 @@ import cv2
 import numpy as np
 import pygame as pg
 pg.mixer.init()
-pg.mixer.music.load('Opening.mp3')
+pg.mixer.music.load('Resursi\Zvučni efekti\Opening.mp3')
 pg.mixer.music.play()
 win=pg.display.set_mode((1920,1080),pg.RESIZABLE)
+a=pg.image.load('Resursi\mario ikona.png')  #ikona
+pg.display.set_icon(a)
 
-cap = cv2.VideoCapture('Super Mario Opening.mp4')
+cap = cv2.VideoCapture('Resursi\Super Mario Opening.mp4')
 if (cap.isOpened()== False): 
   print("Error opening video stream or file")
 
