33d08488e69806aec8540a8e8825122709be6296
Dino Gržinić <dino.grzinic@skole.hr>
2020-04-01 23:31:53 +0200

    Add files via upload

--- videoplayer.py
+++ videoplayer.py
@@ -7,8 +7,8 @@ pg.mixer.music.play()
 win=pg.display.set_mode((1920,1080),pg.RESIZABLE)
 a=pg.image.load('Resursi\mario ikona.png')  #ikona
 pg.display.set_icon(a)
-
 cap = cv2.VideoCapture('Resursi\Super Mario Opening.mp4')
+pg.mixer.music.play()
 if (cap.isOpened()== False): 
   print("Error opening video stream or file")
 
@@ -18,6 +18,7 @@ while(cap.isOpened()):
   if ret == True:
 
     cv2.imshow('Super Mario Opening',frame)
+
 
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
