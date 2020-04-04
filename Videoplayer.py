0e42960c5d300869c633546fe1f7b0f32fb0bd88
Dino Gržinić <dino.grzinic@skole.hr>
2020-04-04 12:39:14 +0200

    Add files via upload

--- Videoplayer.py
+++ Videoplayer.py
@@ -23,17 +23,18 @@ if (cv2.VideoCapture('Custom\Super Mario Opening.mp4').isOpened())==True:
 else:
   x='Resursi\Super Mario Opening.mp4'
 
-if (cv2.VideoCapture('Custom\Super Mario Opening.mp4').isOpened()== False): 
-  print("Error opening video stream or file")
+#if (cv2.VideoCapture('Custom\Super Mario Opening.mp4').isOpened()== False): 
+#  print("Error opening video stream or file")
 
 cap=cv2.VideoCapture(x)
+x=cap.get(cv2.CAP_PROP_FPS)
 
 while(cap.isOpened()):
 
   ret, frame = cap.read()
   if ret == True:
     cv2.imshow('Super Mario Opening',frame)
-    if cv2.waitKey(33) & 0xFF == ord('q'):
+    if cv2.waitKey(int(round((1/x)*1000,0))) & 0xFF == ord('q'):
       pg.mixer.music.pause()
       break
   else: 
