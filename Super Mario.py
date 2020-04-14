f73bc9c4acf7d554c90a34121e7eb4d38c4034ea
Dino Gržinić <dino.grzinic@skole.hr>
2020-04-14 13:51:54 +0200

    Add files via upload

--- Super Mario.py
+++ Super Mario.py
@@ -1,17 +1,15 @@
-#unošenje potrebnih datoteka
 import pygame
 import Videoplayer
-#inicijalizacija Pygame-a
+
 pygame.init()
 pygame.font.init()
-#izgled prozora
+
 win=pygame.display.set_mode((960,540),pygame.RESIZABLE)#dimenzije prozora
 zaslon_dužina=960
 zaslon_visina=540
 a=pygame.image.load('Resursi\mario ikona.png')
 pygame.display.set_icon(a)
 pygame.display.set_caption('Super Mario')
-
 pygame.mixer.init() #zvučni efekti
 pygame.mixer.pre_init(channels=2)
 
@@ -31,14 +29,14 @@ try:
     fireball=pygame.mixer.Sound('Custom/fireball.wav')
 except:
     fireball=pygame.mixer.Sound('Resursi/Zvučni efekti/fireball.wav')
-#izgled fonta
+
 font = pygame.font.SysFont('Comis Sans MS', 32)
-#timer za evente
+
 TIMER_EVENT=pygame.USEREVENT+1 #ubrzavanje gljive
 pygame.time.set_timer(TIMER_EVENT,10000)
 TIMER_EVENT1=pygame.USEREVENT+2 
 pygame.time.set_timer(TIMER_EVENT1,20000)
-#sprite-ovi
+
 dužina_slike=50 # sprite-ovi
 visina_slike=58
 try:
@@ -69,22 +67,22 @@ try:
     pozadina=pygame.image.load('Custom\pozadina.png')
 except:
     pozadina=pygame.image.load('Resursi\pozadina.png')
-#pozicija pozadine
+
 move_x=0
-#brojač pogodaka
+
 brojač_pogodaka=0
-#fps
+
 clock=pygame.time.Clock()
 
 run=True
 
-#podaci za gljivu
+
 class gljiva(object):
-    #animacija hoda   
+    
     gljiva_hod=[pygame.image.load('Resursi\Sprites\gljiva1.png'),pygame.image.load('Resursi\Sprites\gljiva2.png')]
     for i in range (len(gljiva_hod)):
         gljiva_hod[i]=pygame.transform.scale(gljiva_hod[i],(44,40))
-    #vrijednosti za igled i kretanje        
+        
     def __init__(self,x,y,width,height,kraj):
         self.x=x
         self.y=y
@@ -94,7 +92,7 @@ class gljiva(object):
         self.brojač_hoda=0
         self.vel=4
         self.hitbox=(self.x-5,self.y,55,45)
-    
+
     def crtaj(self,win):
         self.move()
         if self.brojač_hoda+1>=90:
@@ -107,10 +105,10 @@ class gljiva(object):
             win.blit(self.gljiva_hod[self.brojač_hoda//45],(self.x,self.y))
             self.brojač_hoda+=1
         self.hitbox=(self.x+2,self.y+2,38,40)
-        #pygame.draw.rect(win,(255,0,0),self.hitbox,2) #zadržao sam hitbox kako bih kasnije znao prilagoditi ga po potrebi
-    #obrazac kretanja gljive
+        #pygame.draw.rect(win,(255,0,0),self.hitbox,2)
+
     def move(self):
-    
+
         if self.vel>0:
             if self.x+self.vel<self.put[1]:
                 self.x+=self.vel
@@ -124,7 +122,7 @@ class gljiva(object):
                 self.vel=self.vel*-1
                 self.x+=self.vel
                 self.brojač_hoda=0       
-#podaci za Maria        
+                   
 class igrač(object):
 
     def __init__(self,x,y,width,height):
@@ -159,7 +157,7 @@ class igrač(object):
                 win.blit(lijevi_hod[0],(self.x,self.y))
         self.hitbox=(self.x+2,self.y,47,62)
         #pygame.draw.rect(win,(0,0,255),self.hitbox,2)
-    #kraj igre
+
     def hit(self):
         win.blit(pozadina,(move_x,0))
         win.blit(neprijatelj.gljiva_hod[1],(neprijatelj.x,neprijatelj.y))
@@ -198,24 +196,22 @@ class projektil(object):  #metci
 def crtanje():  #crtač objekata
     global brojač_hoda
     win.blit(pozadina,(move_x,0))#pozadina
-    
-    Mario.crtaj(win)#lik Maria
-    neprijatelj.crtaj(win)#lik gljive
-    for metak in municija:#metak
+
+    Mario.crtaj(win)
+    neprijatelj.crtaj(win)
+    for metak in municija:
         metak.crtaj(win)
-    pygame.display.update()#osvježavanje prozora
-#pozicija objekata
-Mario=igrač(870,435,35,35)
+    pygame.display.update()
+
+Mario=igrač(800,435,35,35)
 neprijatelj=gljiva(0,453,32,32,960-40)
-#ograničavanje municije
 municija=[]
 shootLoop=1
-
 # glavna petlja
 while run:  
 
     clock.tick(60)   # FPS
-    #određivanje pogotka        
+
     if Mario.hitbox[1]<neprijatelj.hitbox[1]+neprijatelj.hitbox[3] and Mario.hitbox[1]+Mario.hitbox[3]>neprijatelj.hitbox[1]:
         if Mario.hitbox[0]+Mario.hitbox[2]>neprijatelj.hitbox[0] and Mario.hitbox[0]<neprijatelj.hitbox[0]+neprijatelj.hitbox[2]:
             Mario.hit()
@@ -249,32 +245,43 @@ while run:
             
     if tipka[pygame.K_SPACE] and shootLoop==0: # pucanje
         
-        if Mario.lijevo:#smjer metka
+        if Mario.lijevo:
             smjer=-1
             bullet=pygame.image.load('Resursi\Sprites\metak-lijevo.png')
         else:
             bullet=pygame.image.load('Resursi\Sprites\metak-desno.png')
             smjer=1
         if len(municija)<1:
-            fireball.play()#zvučni efekt metka
-            municija.append(projektil(round(Mario.x+Mario.width//2),round(Mario.y+Mario.height//2),6,(0,0,0),smjer))#ograničavanje pucanja
+            fireball.play()
+            municija.append(projektil(round(Mario.x+Mario.width//2),round(Mario.y+Mario.height//2),6,(0,0,0),smjer))
 
-    if tipka[pygame.K_m]: # isključivanje glazbe
+    if tipka[pygame.K_m]: # kontrole
         pygame.mixer.music.pause()
   
-    if tipka[pygame.K_a] and Mario.x>Mario.vel:#kretanje lijevo 
+    if tipka[pygame.K_a] and Mario.x>Mario.vel:
         Mario.x-=Mario.vel
         Mario.lijevo=True
         Mario.desno=False
         Mario.stajanje=False
+        #if Mario.x<=200 and Mario.x>=100:
+            #move_x+=5
+            #move_x2+=5
+            #move_x3+=5
+            #move_x4+=5
+            #move_x5+=5
 
-    elif tipka[pygame.K_d] and Mario.x<zaslon_dužina-Mario.width-Mario.vel:#kretanje desno
+    elif tipka[pygame.K_d] and Mario.x<zaslon_dužina-Mario.width-Mario.vel:
         Mario.x+=Mario.vel
         Mario.lijevo=False
         Mario.desno=True
         Mario.stajanje=False
-
-    else:#stajanje
+        #if Mario.x>=500 and Mario.x<=2800:
+            #move_x-=5
+            #move_x2-=5
+            #move_x3-=5
+            #move_x4-=5
+            #move_x5-=5
+    else:
         Mario.stajanje=True
         Mario.brojač_hoda=0
     if tipka[pygame.K_s] and Mario.y!=435:
@@ -290,7 +297,7 @@ while run:
             Mario.brojač_skoka=16
             jump.play()
 
-    else:#skok u smjeru kretanje igrača
+    else:
         if Mario.brojač_skoka>=-16:
             neg=1
             if Mario.brojač_skoka <0:
@@ -303,16 +310,16 @@ while run:
         else:
             Mario.skok=False
             Mario.bojač_skoka=16
-    #prekid igre pri sudaru gljive i Maria
+
     if Mario.hitbox[1]+Mario.hitbox[3]<neprijatelj.hitbox[1]+neprijatelj.hitbox[3] and Mario.hitbox[1]>neprijatelj.hitbox[1]:
             if Mario.hitbox[0]>neprijatelj.hitbox[0] and Mario.hitbox[0]+Mario.hitbox[2]<neprijatelj.hitbox[0]+neprijatelj.hitbox[2]:
                 run=False
-    #crtanje objekata
+
     crtanje()
-    #prikaz pogodaka
+  
     text = font.render('Broj pogodaka: {}'.format(brojač_pogodaka), True, (255,255,255))
     win.blit(text,(7,7))
-    #ažuriranje prozora
+
     pygame.display.update()
-#kraj programa
+
 pygame.quit()
--- Videoplayer.py
+++ Videoplayer.py
@@ -1,43 +1,50 @@
-#unošenje potrebnih python knjižnica
 import cv2
 import numpy as np
 import pygame as pg
-#inicijalizacija pygame-a
+import random
+
+for i in range (1):
+    y=random.randint(1,2)
+
 pg.init()
 pg.mixer.init()
-#izgled prozora
 a=pg.image.load('Resursi\mario ikona.png')
 pg.display.set_icon(a)
 win=pg.display.set_mode((1920,1080),pg.RESIZABLE)
 pg.display.set_caption('Super Mario Opening')
-#zvuk videa
+
 try:
   pg.mixer.music.load('Custom\Opening.mp3')
 
-except: 
-  pg.mixer.music.load('Resursi\Zvučni efekti\Opening.mp3')
-
+except:
+  if y==1:
+    pg.mixer.music.load('Resursi\Zvučni efekti\Opening.mp3')
+  else:
+    pg.mixer.music.load('Resursi\Zvučni efekti\Opening2.mp3')
 pg.mixer.music.play()
-#otvaranje video file-a
+
 if (cv2.VideoCapture('Custom\Opening.mp4').isOpened())==True:
   x='Custom\Opening.mp4'
 
 else:
-  x='Resursi\Opening.mp4'
+  if y==1:
+    x='Resursi\Opening.mp4'
+  else:
+    x='Resursi\Opening2.mp4'
 
 cap=cv2.VideoCapture(x)
 x=cap.get(cv2.CAP_PROP_FPS)
-#puštanje svakog frame-a videa određenu količinu vremena do puštanja idućeg
+
 while(cap.isOpened()):
 
   ret, frame = cap.read()
   if ret == True:
     cv2.imshow('Super Mario Opening',frame)
-    if cv2.waitKey(int(round((1/x)*1000,0))-10) & 0xFF == ord('q'): #zatvaranje prozora tipkom Q
+    if cv2.waitKey(int(round((1/x)*1000,0))-12) & 0xFF == ord('q'):
       pg.mixer.music.pause()
       break
   else: 
     break
-#zatvaranje prozora
+
 cap.release()
 cv2.destroyAllWindows()
